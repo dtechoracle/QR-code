c8b09cf67e744e45091dafeb5b3cb8f501060307
dtechoracle <69088988+dtechoracle@users.noreply.github.com>
2023-02-05 07:53:01 -1200

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
 <div className="section container">
       <QrCode />
+    
+    <p>Made with <span>&#10084</span> by <a href="twitter.com/dtechoracle">dtechoracle</a></p>
     </div>
   );
 }
